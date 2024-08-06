bd28d88c2cc14d694885aff07f3a32306dad96b5
Aiden-Coding <dwx_job@163.com>
2024-08-07 00:08:04 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Menu from "@/components/app_bar";
+import Menu from "@/components/app-bar";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
