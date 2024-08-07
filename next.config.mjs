063130844c62915bad54817f09c28664bfd56830
Aiden-Coding <dwx_job@163.com>
2024-08-07 19:59:03 +0800

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    swcMinify: true,  // 显式启用 SWC 压缩
-    compiler: {
-        styledComponents: true,  // 示例：启用 styled-components 支持
-    },
+    output: 'standalone'
 };
 
 export default nextConfig;
